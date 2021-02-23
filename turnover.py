ea4e07e95ec4e0564d09989c8021b02b248f8479
Rob Curtis <waab@me.com>
2021-02-23 08:55:47 -0500

    Add placeholders for actions/items in turnover

--- turnover.py
+++ turnover.py
@@ -11,11 +11,25 @@ logging.basicConfig(filename='PhaseTurnover.log', level=logging.INFO,
 
 def handle_turnover(phase):
     illuminatus_sheet = MySheet('Illuminatus! Test Sheet')
-    votes, inactive = tabulate_votes(illuminatus_sheet, phase)
-    for votee in votes.keys():
-        print('%s received %s votes: %s' %(votee, len(votes[votee]), votes[votee]))
-        
+    
+    do_actions(illuminatus_sheet, phase)
+    
+    do_items(illuminatus_sheet, phase)
+    
+    do_votes(illuminatus_sheet, phase)
+
+def do_votes(sheet, phase):
+    votes_received, inactive = tabulate_votes(sheet, phase)
+    for target in votes_received.keys():
+        print('%s received %s votes: %s' % (target, len(votes_received[target]), votes_received[target]))
     print('Non-voting players: %s' % inactive)
+    
+def do_actions(sheet, phase):
+    print('No actions for phase [%s]' % phase)
+
+def do_items(sheet, phase):
+    print('No items for phase [%s]' % phase)
+
     
 if __name__ == "__main__":
     phase = sys.argv[1]
